fbd13ccf2385756d155da7f3804468669bd8992c
magrosskopf <magrosskopf@web.de>
2018-10-08 11:51:33 +0200

    Initial commit

---
+++ src/com/MaGrosskopf/Main.java
@@ -0,0 +1,9 @@
+package com.MaGrosskopf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hallo Zoo");
+    }
+}
---
+++ src/com/MaGrosskopf/UnitTests.java
@@ -0,0 +1,18 @@
+package com.MaGrosskopf;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTests {
+
+    @Test
+    public void ErsterTest(){
+        assertEquals(1,1);
+
+    }
+
+
+
+}
